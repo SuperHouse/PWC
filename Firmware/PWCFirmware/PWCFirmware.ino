5d725967e7168e6e3473a01ab8c93204584ea0f9
Jonathan Oxer <jon@oxer.com.au>
2021-06-23 13:57:41 +1000

    Preliminary support for PS4 (BLE) game controller as input. Made input type selectable in config. Made debugging more configurable in config.

--- Firmware/PWCFirmware/PWCFirmware.ino
+++ Firmware/PWCFirmware/PWCFirmware.ino
@@ -22,8 +22,14 @@
                        v
                      -100%
 
-   Internal dependencies. Install using Arduino library manager:
+   External dependencies. Install using Arduino library manager:
      "SparkFun Qwiic Scale NAU7802" by SparkFun Electronics
+
+   External dependencies. Install by downloading the ZIP file:
+     "PS4-esp32" by aed3 https://github.com/aed3/PS4-esp32
+     It may need to be this specific version, if the latest version
+     above fails to read values from the controller:
+     https://github.com/aed3/PS4-esp32/tree/d23311c8489f2538c8e49d6e39dc33b19e0edbcb
 
    More information:
      www.superhouse.tv/pwc
@@ -52,15 +58,20 @@
 
    Copyright 2021 SuperHouse Automation Pty Ltd www.superhouse.tv
 */
-#define VERSION "1.0"
+#define VERSION "2.0"
 /*--------------------------- Configuration ---------------------------------*/
 // Configuration should be done in the included file:
 #include "config.h"
 
 /*--------------------------- Libraries -------------------------------------*/
 #include <Wire.h>
-#include "SparkFun_Qwiic_Scale_NAU7802_Arduino_Library.h" // Load cell amplifier
 #include <RunningMedian.h>          // By Rob Tillaart
+#if ENABLE_ZEROSTICK
+#include "SparkFun_Qwiic_Scale_NAU7802_Arduino_Library.h" // Load cell amplifier
+#endif
+#if ENABLE_PS4_CONTROLLER
+#include <PS4Controller.h>          // By aed3
+#endif
 
 #if ENABLE_DIGIPOT_OUTPUT
 #include <Adafruit_DS3502.h>        // Digital potentiometer
@@ -87,7 +98,9 @@ int32_t  g_sensor_raw_value_sum  = 0;   // Accumulate channel readings
 
 /*--------------------------- Instantiate Global Objects --------------------*/
 // Load cells
+#if ENABLE_ZEROSTICK
 NAU7802 loadcells;
+#endif
 
 RunningMedian x_samples = RunningMedian(5);
 RunningMedian y_samples = RunningMedian(5);
@@ -107,8 +120,14 @@ void setup()
   //while (!Serial) {
   //  ; // wait for serial port to connect. Needed for native USB port only
   //}
+  Serial.println();
   Serial.print("Permobil Wheelchair Controller starting up, v");
   Serial.println(VERSION);
+
+#ifdef ARDUINO_ARCH_ESP32
+  Serial.println("Architecture detected: ESP32");
+#endif
+  delay(5000);
 
   Wire.begin();
 
@@ -116,54 +135,72 @@ void setup()
   pinMode(TARE_BUTTON_PIN,  INPUT_PULLUP);
 
 #if ENABLE_DIGIPOT_OUTPUT
-  if (!digipot_x.begin(DIGIPOT_X_I2C_ADDR)) {
+  Serial.println("Searching for output digipots:");
+  if (!digipot_x.begin(DIGIPOT_X_I2C_ADDR))
+  {
     Serial.println("Couldn't find X-axis DS3502 chip");
   }
   Serial.println("Found X-axis DS3502 chip");
 
-  if (!digipot_y.begin(DIGIPOT_Y_I2C_ADDR)) {
+  if (!digipot_y.begin(DIGIPOT_Y_I2C_ADDR))
+  {
     Serial.println("Couldn't find Y-axis DS3502 chip");
   }
   Serial.println("Found Y-axis DS3502 chip");
 
-  digipot_x.setWiper(DIGIPOT_CENTER);               // Start with pots in central position
-  digipot_y.setWiper(DIGIPOT_CENTER);               // Start with pots in central position
+  digipot_x.setWiper(DIGIPOT_CENTER);        // Start with pots in central position
+  digipot_y.setWiper(DIGIPOT_CENTER);        // Start with pots in central position
 #endif
 
+#if ENABLE_ZEROSTICK
   // Start the load cell interface
-#if ENABLE_INPUT_DEBUGGING
+#if ENABLE_ZEROSTICK_DEBUGGING
   Serial.print("Starting load cell interface: ");
-#endif
+#endif ENABLE_ZEROSTICK_DEBUGGING
   if (loadcells.begin() == false)
   {
-#if ENABLE_INPUT_DEBUGGING
+#if ENABLE_ZEROSTICK_DEBUGGING
     Serial.println("Not detected. Halting.");
-#endif
+#endif ENABLE_ZEROSTICK_DEBUGGING
     while (1);
   }
-#if ENABLE_INPUT_DEBUGGING
+#if ENABLE_ZEROSTICK_DEBUGGING
   Serial.println("ok.");
-#endif
+#endif ENABLE_ZEROSTICK_DEBUGGING
   loadcells.setGain(NAU7802_GAIN_1);        // Gain can be set to 1, 2, 4, 8, 16, 32, 64, or 128.
   loadcells.setSampleRate(NAU7802_SPS_320); // Sample rate can be set to 10, 20, 40, 80, or 320Hz
   delay(100);
   tareCellReadings();
+#endif ENABLE_ZEROSTICK
+
+#if ENABLE_PS4_CONTROLLER
+  PS4.begin(BLE_MAC);
+#endif ENABLE_PS4_CONTROLLER
 }
+
 
 /**
   Loop
 */
 void loop()
 {
+#if ENABLE_ZEROSTICK
   checkTareButton();
-  readInputPosition();
+  readZerostickInputPosition();
+#endif ENABLE_ZEROSTICK
+
+#if ENABLE_PS4_CONTROLLER
+  readPs4InputPosition();
+#endif ENABLE_PS4_CONTROLLER
 
   updateDigipotOutputs();
 }
 
+
 /**
   Check if the tare button is pressed.
 */
+#if ENABLE_ZEROSTICK
 void checkTareButton()
 {
   if (LOW == digitalRead(TARE_BUTTON_PIN))
@@ -174,7 +211,7 @@ void checkTareButton()
     delay(100);
   }
 }
-
+#endif ENABLE_ZEROSTICK
 
 /**
   Send joystick movements to a pair of digital potentiometers using
@@ -226,7 +263,8 @@ void updateDigipotOutputs()
 /**
   Read the sensor pressure levels
 */
-void readInputPosition()
+#if ENABLE_ZEROSTICK
+void readZerostickInputPosition()
 {
   //Serial.println("Reading");
 
@@ -283,17 +321,19 @@ void readInputPosition()
       loadcells.calibrateAFE();
     }
 
-#if ENABLE_INPUT_DEBUGGING
+#if ENABLE_ZEROSTICK_DEBUGGING
     Serial.print(g_input_x_position);
     Serial.print("  ");
     Serial.println(g_input_y_position);
-#endif
+#endif ENABLE_ZEROSTICK_DEBUGGING
   }
 }
+#endif ENABLE_ZEROSTICK
 
 /**
   Reset the zero position of the load cells. Select each channel in turn and tare them.
 */
+#if ENABLE_ZEROSTICK
 void tareCellReadings()
 {
   loadcells.setChannel(0);
@@ -301,28 +341,81 @@ void tareCellReadings()
   loadcells.calculateZeroOffset(64);        // Tare operation, averaged across 64 readings
   //loadcells.setCalibrationFactor(g_x_calibration_factor);
   g_x_zero_offset = loadcells.getZeroOffset();
-#if ENABLE_INPUT_DEBUGGING
+#if ENABLE_ZEROSTICK_DEBUGGING
   Serial.print("X zero offset: ");
   Serial.println(loadcells.getZeroOffset());
   Serial.print("X calibration factor: ");
   Serial.println(loadcells.getCalibrationFactor());
-#endif
+#endif ENABLE_ZEROSTICK_DEBUGGING
 
   loadcells.setChannel(1);
   loadcells.calibrateAFE();                 // Internal calibration. Recommended after power up, gain changes, sample rate changes, or channel changes.
   loadcells.calculateZeroOffset(64);        // Tare operation, averaged across 64 readings
   //loadcells.setCalibrationFactor(g_y_calibration_factor);
   g_y_zero_offset = loadcells.getZeroOffset();
-#if ENABLE_INPUT_DEBUGGING
+#if ENABLE_ZEROSTICK_DEBUGGING
   Serial.print("Y zero offset: ");
   Serial.println(loadcells.getZeroOffset());
   Serial.print("Y calibration factor: ");
   Serial.println(loadcells.getCalibrationFactor());
-#endif
+#endif ENABLE_ZEROSTICK_DEBUGGING
 
   loadcells.setChannel(g_next_channel_to_read); // To leave the selected channel in the correct state
 
-#if ENABLE_INPUT_DEBUGGING
+#if ENABLE_ZEROSTICK_DEBUGGING
   Serial.println("Tare complete");
-#endif
+#endif ENABLE_ZEROSTICK_DEBUGGING
 }
+#endif
+
+
+#if ENABLE_PS4_CONTROLLER
+/**
+   Read the right joystick from a paired PS4 controller (BLE)
+*/
+void readPs4InputPosition()
+{
+  if (PS4.isConnected())
+  {
+    // Process the X axis
+    if (PS4.RStickX())
+    {
+      float x_position = PS4.RStickX(); // What type of values do we get? Decimals?
+#if ENABLE_PS4_DEBUGGING
+      Serial.printf("Right stick X at %d\n", x_position);
+#endif ENABLE_PS4_DEBUGGING
+      g_input_x_position = map(x_position, -PS4_FULL_SCALE, PS4_FULL_SCALE, -100, 100); // Adjust to a percentage of full force
+      g_input_x_position = constrain(g_input_x_position, -100, 100);          // Prevent going out of bounds
+
+      if (INPUT_DEAD_SPOT_SIZE < g_input_x_position)
+      {
+        g_input_x_position -= INPUT_DEAD_SPOT_SIZE;
+      } else if (-1 * INPUT_DEAD_SPOT_SIZE > g_input_x_position) {
+        g_input_x_position += INPUT_DEAD_SPOT_SIZE;
+      } else {
+        g_input_x_position = 0;
+      }
+    }
+
+    // Process the Y axis
+    if (PS4.RStickY())
+    {
+      float y_position = PS4.RStickY(); // What type of values do we get? Decimals?
+#if ENABLE_PS4_DEBUGGING
+      Serial.printf("Right stick Y at %d\n", y_position);
+#endif ENABLE_PS4_DEBUGGING
+      g_input_y_position = map(y_position, -PS4_FULL_SCALE, PS4_FULL_SCALE, -100, 100); // Adjust to a percentage of full force
+      g_input_y_position = constrain(g_input_y_position, -100, 100);          // Prevent going out of bounds
+
+      if (INPUT_DEAD_SPOT_SIZE < g_input_y_position)
+      {
+        g_input_y_position -= INPUT_DEAD_SPOT_SIZE;
+      } else if (-1 * INPUT_DEAD_SPOT_SIZE > g_input_y_position) {
+        g_input_y_position += INPUT_DEAD_SPOT_SIZE;
+      } else {
+        g_input_y_position = 0;
+      }
+    }
+  }
+}
+#endif ENABLE_PS4_CONTROLLER
